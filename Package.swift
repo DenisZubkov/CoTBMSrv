e3424892fbbd13a5716965c3012a78c39a5da655
Denis Zubkov <denzu@mac.com>
2019-10-24 08:58:44 +0300

    Delete ZIPFoundation

--- Package.swift
+++ Package.swift
@@ -8,10 +8,9 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMinor(from: "3.0.0")),
         .package(url: "https://github.com/vapor/fluent-mysql.git", .upToNextMinor(from: "3.0.0")),
-        .package(url: "https://github.com/weichsel/ZIPFoundation/", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentSQLite", "FluentMySQL", "ZIPFoundation"]),
+        .target(name: "App", dependencies: ["Vapor", "FluentSQLite", "FluentMySQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ])
